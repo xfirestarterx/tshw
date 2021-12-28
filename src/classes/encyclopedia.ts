617ef550e5e41f06e5a0a399f89b4ffaca3bb870
a <t@t.t>
2021-12-28 18:04:15 +0200

    task8 done

--- src/classes/encyclopedia.ts
+++ src/classes/encyclopedia.ts
@@ -1,6 +1,18 @@
+import { positiveInteger } from "../decorators";
 import { ReferenceItem } from "./referenceItem";
 
 export default class Encyclopedia extends ReferenceItem {
+  private _copies: number;
+
+  get copies(): number {
+    return this._copies;
+  }
+
+  @positiveInteger
+  set copies(val) {
+    this._copies = val;
+  }
+
   constructor(title: string, year: number, id: number, public edition: number) {
     super(title, year, id);
   }
--- src/classes/universityLibrarian.ts
+++ src/classes/universityLibrarian.ts
@@ -1,9 +1,25 @@
+import { format, logger, logMethod, logParameter, sealed, writable } from '../decorators';
 import * as Interfaces from '../interfaces';
 
+// @logger
+// @sealed('UniversityLibrarian')
 export class UniversityLibrarian implements Interfaces.ILibrarian {
-  constructor (public department: string, public name: string, public email: string) {}
+  @format() name: string;
 
-  assistCustomer(custName: string) {
+  constructor (public department: string, name: string, public email: string) {}
+
+  @logMethod
+  assistCustomer(@logParameter custName: string) {
     console.log(`${ this.name } assists ${ custName }`);
+  }
+
+  // @writable(true)
+  assistFaculty() {
+    console.log('assist faculty')
+  }
+
+  // @writable(false)
+  teachCommunity() {
+    console.log('teaching')
   }
 }
